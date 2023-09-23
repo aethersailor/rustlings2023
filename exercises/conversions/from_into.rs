747074d5676fc62ba5acfaec7a4bc3417589f225
Raen <raen@quantumtrip.org>
2023-09-22 22:37:54 -0400

    Finished conversions and Rustlings

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -40,10 +40,34 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
 
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        
+        let s_split: Vec<&str> = s.split(",").collect();
+        if s_split.len() != 2 {
+            return Person::default();
+        }
+
+        let name = s_split[0].to_string();
+        if name.len() == 0 {
+            return Person::default();
+        }
+        
+        match s_split[1].parse::<usize>() {
+            Ok(age) => {
+                return Person {
+                    name: name,
+                    age: age
+                }
+            },
+            Err(_) => {
+                return Person::default()
+            },
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,32 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+
+        let s_split: Vec<&str> = s.split(",").collect();
+        if s_split.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+
+        let name = s_split[0];
+        if name.len() == 0 {
+            return Err(ParsePersonError::NoName);
+        }
+
+        match s_split[1].parse::<usize>() {
+            Ok(age) => {
+                return Ok(Person {
+                    name: name.to_string(),
+                    age: age
+                });
+            },
+            Err(e) => {
+                return Err(ParsePersonError::ParseInt(e));
+            }
+        }
+
     }
 }
 
